0b474542ec47f06084cad11b05633771be343212
Kenneth <kennethwty93@gmail.com>
2019-06-29 11:07:57 -0400

    Finish CRUD for message resource

--- src/main/java/hk/com/kennethwty/messenger/resources/MessageResource.java
+++ src/main/java/hk/com/kennethwty/messenger/resources/MessageResource.java
@@ -2,7 +2,11 @@ package hk.com.kennethwty.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,20 +16,38 @@ import hk.com.kennethwty.messenger.model.Message;
 import hk.com.kennethwty.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
 	
+	@POST
+	public Message addMessage(Message msg) {
+		return messageService.addMessage(msg);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message msg) {
+		msg.setId(id);
+		return messageService.updateMessage(msg);
+	}
+	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
 	public Message getMessage(@PathParam("messageId") long msgId) {
 		return messageService.getMessage(msgId);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void updateMessage(@PathParam("messageId") long id) {
+		messageService.deleteMessage(id);
 	}
 }
